efac622f215ab63c2824d513d12354b6e75edfba
jeremiah-norris-champ <jeremiah.norris@champtitles.com>
2021-08-03 16:55:04 -0500

    CLOUD-30 add sed and examples

--- action.sh
+++ action.sh
@@ -2,8 +2,8 @@
 
 export DIRECTORY=${DIRECTORY:-.update}
 export FILE=${FILE:-variables.tf}
-export PATTERN=${PATTERN:-lkajsdflkajsdflkjsadf}
 export BRANCH=${BRANCH:-develop}
+export SUFFIX=${BRANCH:-\"}
 
 if [ -z "${SSH_PRIVATE_KEY}" ]; then
     echo "SSH_PRIVATE_KEY is required to clone private repositories"
@@ -15,12 +15,12 @@ if [ -z "${TARGET_REPO}" ]; then
     exit 1
 fi
 
-if [ -z "${TARGET_KEY}" ]; then
-    export TARGET_KEY=$(echo ${GITHUB_REPOSITORY} | sed -e 's|.*/||')
+if [ -z "${SEARCH_KEY}" ]; then
+    export SEARCH_KEY="$(echo ${GITHUB_REPOSITORY} | sed -e 's|.*/||'):"
 fi
 
-if [ -z "${TARGET_VALUE}" ]; then
-    export TARGET_KEY=${GITHUB_SHA}
+if [ -z "${REPLACE_VALUE}" ]; then
+    export REPLACE_VALUE=${GITHUB_SHA}
 fi
 
 # enable cloning of private repositories
@@ -29,11 +29,23 @@ ssh-keyscan -t rsa github.com >>~/.ssh/known_hosts
 eval $(ssh-agent)
 ssh-add - <<<"${SSH_PRIVATE_KEY}"
 
-# update git repository
+# clone target git repository
 git clone -b ${BRANCH} ${TARGET_REPO} ${DIRECTORY}
 cd ${DIRECTORY}
-sed ${FILE} ${TARGET_KEY} ${PATTERN}
-git commit -m "${TARGET_KEY} ${TARGET_VALUE}"
+
+if [ ! -f "${FILE}" ]; then
+    echo "File ${FILE} does not exist"
+    exit 1
+fi
+
+if [ -z "$(cat ${FILE} | grep ${SEARCH_KEY})" ]; then
+    echo "SEARCH_KEY ${SEARCH_KEY} was not found in ${FILE}"
+    exit 1
+fi
+
+# update the value
+sed -i "s/${SEARCH_KEY}.*/${SEARCH_KEY}${REPLACE_VALUE}${SUFFIX}/g" ${FILE}
+git commit -m "${SEARCH_KEY}${REPLACE_VALUE}"
 
 # retry logic to mitigate race conditions between multiple repositories
 for i in 1 2 3 4 5; do git push origin ${BRANCH} && break || git pull -r && sleep 5; done
